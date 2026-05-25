9a23d72e82df8f4f29185bbfeb1fa8d56f7d7716
rajeshturing <rajesh.s@turing.com>
2026-05-25 09:33:53 +0530

    Add Cocoon login diagnostics

--- navo.cocoon/navo.cocoon.ebwrap/ne_connection.vb
+++ navo.cocoon/navo.cocoon.ebwrap/ne_connection.vb
@@ -5,23 +5,41 @@
 '//--------------------------------------------------------------------------
 
 Imports System.Runtime.InteropServices
+Imports System.IO
 Public Class ne_connection
     Implements IDisposable
 
     Public Sub create(ByVal strApplication As String, ByVal strDBServer As String, ByVal strDBName As String, ByVal iUIStyle As Int32, ByVal bNXMTransport As Boolean)
-        m_oClient = CreateObject("navo2002.client")
-        m_oClientDoc = m_oClient.document
+        Trace("ne_connection.create start app=" & strApplication & " db=" & strDBName & " server=" & strDBServer & " ui=" & CStr(iUIStyle) & " nxm=" & CStr(bNXMTransport))
+        Try
+            Trace("CreateObject navo2002.client begin")
+            m_oClient = CreateObject("navo2002.client")
+            Trace("CreateObject navo2002.client ok null=" & CStr(m_oClient Is Nothing))
 
-        Dim str As String = "vn1001=1030 vn1011=1 vn1002=72007 vn1016="
+            Trace("client.document begin")
+            m_oClientDoc = m_oClient.document
+            Trace("client.document ok null=" & CStr(m_oClientDoc Is Nothing))
 
-        str = str & CStr(iUIStyle)
+            Dim str As String = "vn1001=1030 vn1011=1 vn1002=72007 vn1016="
 
-        If bNXMTransport Then
-            str = (str & " vs8080='nxm'")
-        End If
-        str = String.Concat(New String() {str, " vs8003='", strApplication, "' vs8008='", strDBName, "' vs1012='", strDBServer, "'"})
-        m_oClientDoc.callcommand(str)
-        m_oTopPage = m_oClientDoc.gettoppage
+            str = str & CStr(iUIStyle)
+
+            If bNXMTransport Then
+                str = (str & " vs8080='nxm'")
+            End If
+            str = String.Concat(New String() {str, " vs8003='", strApplication, "' vs8008='", strDBName, "' vs1012='", strDBServer, "'"})
+
+            Trace("callcommand begin " & str)
+            m_oClientDoc.callcommand(str)
+            Trace("callcommand ok")
+
+            Trace("gettoppage begin")
+            m_oTopPage = m_oClientDoc.gettoppage
+            Trace("gettoppage ok null=" & CStr(m_oTopPage Is Nothing))
+        Catch ex As Exception
+            Trace("ne_connection.create exception " & ex.ToString())
+            Throw
+        End Try
     End Sub
     Public Sub Dispose() Implements IDisposable.Dispose
         Me.Dispose(True)
@@ -56,6 +74,17 @@ Public Class ne_connection
     Public Function createhli() As ne_property_set
         Return New ne_property_set(m_oClientDoc.createhli())
     End Function
+
+    Private Shared Sub Trace(ByVal strMessage As String)
+        Try
+            Dim strPath As String = Environment.GetEnvironmentVariable("NAVO_COCOON_DIAG_LOG")
+            If String.IsNullOrWhiteSpace(strPath) Then
+                strPath = "C:\app\cocoon-diagnostics.log"
+            End If
+            File.AppendAllText(strPath, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") & " [ne_connection] " & strMessage & Environment.NewLine)
+        Catch
+        End Try
+    End Sub
 
     Private m_oClient As Object
     Private m_oClientDoc As Object
--- navo.cocoon/navo.cocoon.webapi/Controllers/login_controller.cs
+++ navo.cocoon/navo.cocoon.webapi/Controllers/login_controller.cs
@@ -6,6 +6,8 @@
 
 using navo.cocoon.data;
 using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Web.Http;
 
 namespace navo.cocoon.webapi
@@ -16,14 +18,26 @@ namespace navo.cocoon.webapi
         [Route("api/v4/login")]
         public login_result Login()
         {
-            api_controller_plus oController = new api_controller_plus(this);
+            Trace("login request begin");
+            try
+            {
+                api_controller_plus oController = new api_controller_plus(this);
 
-            string strJSON = Request.Content.ReadAsStringAsync().Result;
-            login_request oLogin = JsonConvert.DeserializeObject<login_request>(strJSON);
+                string strJSON = Request.Content.ReadAsStringAsync().Result;
+                Trace("login json read length=" + (strJSON == null ? "null" : strJSON.Length.ToString()));
+                login_request oLogin = JsonConvert.DeserializeObject<login_request>(strJSON);
+                Trace("login parsed app=" + oLogin?.application_name + " db=" + oLogin?.database + " server=" + oLogin?.eb_server + " account=" + oLogin?.account_id + " mode=" + oLogin?.mode + " ui=" + oLogin?.ui_style);
 
-            login_result oResult = eb_client_manager.manager.create_token_for_login(oLogin);
+                login_result oResult = eb_client_manager.manager.create_token_for_login(oLogin);
+                Trace("login result category=" + oResult?.user_category + " user=" + oResult?.user_name + " token_empty=" + string.IsNullOrWhiteSpace(oResult?.token));
 
-            return oResult;
+                return oResult;
+            }
+            catch (Exception ex)
+            {
+                Trace("login exception " + ex);
+                throw;
+            }
         }
 
         [Route("api/v4/logoff")]
@@ -32,6 +46,22 @@ namespace navo.cocoon.webapi
             api_controller_plus oController = new api_controller_plus(this);
 
             return eb_client_manager.manager.release_token(oController.get_ncct()) ? null : api_controller_plus.ERROR_MISSING_INVALID_NCCT;
+        }
+
+        private static void Trace(string message)
+        {
+            try
+            {
+                string path = Environment.GetEnvironmentVariable("NAVO_COCOON_DIAG_LOG");
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    path = @"C:\app\cocoon-diagnostics.log";
+                }
+                File.AppendAllText(path, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " [login_controller] " + message + Environment.NewLine);
+            }
+            catch
+            {
+            }
         }
     }
 }
